dfb3d9f2e93adee2000dd3fda7ba1b59ba5f51c5
anblackter <angel.henao@outlook.com>
2024-04-23 16:07:20 -0500

    feat(basics assigment-1): assignment solution

---
+++ 02-section-basics-variables-data-types-operators-functions/basics-assignment-1-start/assignment.js
@@ -0,0 +1,10 @@
+const userInput = 7;
+let result;
+
+result = userInput + 18;
+result = result - 5;
+result = result * 3;
+result = result / 1;
+
+alert(`The result: ${result}`);
+alert(`The user input: ${userInput}`)
