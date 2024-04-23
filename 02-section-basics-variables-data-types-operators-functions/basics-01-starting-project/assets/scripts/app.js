5ce636c720090217e28e1e70a900d177c5cd6cea
anblackter <angel.henao@outlook.com>
2024-04-23 17:59:06 -0500

    feat(basics 01-starting-project): add functions and eventListener to display the output

--- 02-section-basics-variables-data-types-operators-functions/basics-01-starting-project/assets/scripts/app.js
+++ 02-section-basics-variables-data-types-operators-functions/basics-01-starting-project/assets/scripts/app.js
@@ -3,8 +3,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = currentResult + 10 * 3;
+function add(num1, num2) {
+  currentResult = currentResult + userInput.value;
+  outputResult(currentResult, '');
+}
 
-let calculationDescription = `(${defaultResult} + 10 * 3)`
-
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener('click', add);
