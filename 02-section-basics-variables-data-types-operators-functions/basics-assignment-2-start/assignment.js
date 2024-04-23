cf24fb5c47654f45f7295d25f4f9052b7cd3f5b8
anblackter <angel.henao@outlook.com>
2024-04-23 18:07:16 -0500

    feat(basics assignment-2): assignment 2 solution

--- 02-section-basics-variables-data-types-operators-functions/basics-assignment-2-start/assignment.js
+++ 02-section-basics-variables-data-types-operators-functions/basics-assignment-2-start/assignment.js
@@ -1,1 +1,23 @@
 const task3Element = document.getElementById('task-3');
+
+function noParameter(){
+  alert('Function with no parameter');
+}
+
+function withParameter(text){
+  alert(`Received parameter: ${text}`);
+}
+
+noParameter()
+
+withParameter('Hello World!')
+
+
+task3Element.addEventListener('click', noParameter);
+
+function combination(text1, text2, text3){
+  const result = text1 + text2 + text3
+  alert(`The combination of the three texts are: ${result}`)
+}
+
+combination('hello', 'world', 'JS')
