00c0830d28ce58acfaacb1affeb2aa9ee97ba37c
anblackter <angel.henao@outlook.com>
2024-04-23 14:33:15 -0500

    feat(basics 01-starting project): add template literal, formatted text

--- 02-section-basics-variables-data-types-operators-functions/basics-01-starting-project/assets/scripts/app.js
+++ 02-section-basics-variables-data-types-operators-functions/basics-01-starting-project/assets/scripts/app.js
@@ -5,6 +5,6 @@ let currentResult = defaultResult;
 
 currentResult = currentResult + 10 * 3;
 
-let calculationDescription = '(' + defaultResult + ' + 10 * 3)'
+let calculationDescription = `(${defaultResult} + 10 * 3)`
 
 outputResult(currentResult, calculationDescription);
