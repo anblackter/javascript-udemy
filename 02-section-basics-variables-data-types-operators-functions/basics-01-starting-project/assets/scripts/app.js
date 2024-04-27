ea4a596450a935af16046b654f6ac0c7d51c13bb
anblackter <angel.henao@outlook.com>
2024-04-27 11:04:01 -0500

    feat(basics 01-starting-project): include multiply and divide functions, also register the events to the buttons

--- 02-section-basics-variables-data-types-operators-functions/basics-01-starting-project/assets/scripts/app.js
+++ 02-section-basics-variables-data-types-operators-functions/basics-01-starting-project/assets/scripts/app.js
@@ -19,11 +19,28 @@ function add() {
   createAndWriteLog('+', initialResult, enteredNumber)
 }
 
-function substract() {
+function subtract() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
   currentResult = currentResult - enteredNumber;
   createAndWriteLog('-', initialResult, enteredNumber)
 }
 
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWriteLog('*', initialResult, enteredNumber)
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWriteLog('/', initialResult, enteredNumber)
+}
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
