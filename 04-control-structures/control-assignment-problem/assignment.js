19b5a8d2913f714d5dbecd3a2019198edbee2603
Angel Henao <ay.henao@uniandes.edu.co>
2024-09-09 01:16:22 +0000

    Assignment solution

--- 04-control-structures/control-assignment-problem/assignment.js
+++ 04-control-structures/control-assignment-problem/assignment.js
@@ -1,1 +1,23 @@
 const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
+
+if (randomNumber > 0.7) {
+  alert('Greater than 0.7');
+}
+
+numbersArray = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+
+for (let i = 0; i < numbersArray.length; i++) {
+  console.log(numbersArray[i]);
+}
+
+for (const number of numbersArray) {
+  console.log(number);
+}
+
+const secondRandomNumber = Math.random();
+
+if (secondRandomNumber > 0.7 && randomNumber > 0.7) {
+  alert('Both greater than 0.7');
+} else if (secondRandomNumber < 0.2 || randomNumber < 0.2) {
+  alert('Both less than 0.2');
+}
