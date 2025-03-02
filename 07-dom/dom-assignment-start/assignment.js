eb11a06a4fa850d1e8beab54a0c8b45cd10b9b9b
Angel Henao <angel.henao@reorg.com>
2025-03-01 21:15:37 -0500

    DOM assignment by myself and suggested solution

---
+++ 07-dom/dom-assignment-start/assignment.js
@@ -0,0 +1,10 @@
+const selectionWayOne = document.querySelector('body');
+const selectionWayTwo = document.body;
+
+selectionWayOne.setAttribute('style', 'background: black; color: white;');
+
+const title = document.querySelector('head title');
+title.textContent = 'Assignment - Solved!';
+
+const h1 = document.querySelector('h1');
+h1.textContent = 'Assignment - Solved!';
